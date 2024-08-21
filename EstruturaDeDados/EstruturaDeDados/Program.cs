7e1d261ca5d7e908c30a99f1c227881be8e8f3e1
GabrielaSiqCap <gabrielasiqueiracaputti@gmail.com>
2024-08-21 14:53:18 -0300

    Finalizando os exemplos list

--- EstruturaDeDados/EstruturaDeDados/Program.cs
+++ EstruturaDeDados/EstruturaDeDados/Program.cs
@@ -1,9 +1,75 @@
-﻿internal class Program
+﻿using System.Reflection.Metadata.Ecma335;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Minha primeira branch!");
-        Console.WriteLine("Trabalhando com Git\\GitHub");
-        Console.WriteLine("Mais uma tentativa de trabalhar com Git");
+        // TRABALHANDO COM LISTA (LIST)
+        // Criando uma lista
+        List<string> frutas = new List<string>();
+
+        //Adicionar itens na lista
+        frutas.Add("Morango");
+        frutas.Add("Manga");
+        frutas.Add("Goiaba");
+        frutas.Add("Uva");
+
+        // Imprimir os Itens da Lista
+        Console.WriteLine("Minha lista de frutas:");
+        foreach (string fruta in frutas)
+        {
+            Console.WriteLine(fruta);
+        }
+
+        Console.WriteLine(); //Pula linha em Branco
+
+        // Imprimir elemento no índice específico
+        Console.WriteLine("Fruta na indice 2:" + frutas[2]); //obs: índice sempre começa em 0
+
+
+        // Inserindo um elemento no índice específico -> Adição 
+        frutas.Insert(1, "Maracujá");
+
+        Console.WriteLine(); //Pula linha em Branco
+
+        // Impressão da lista em uma única linha
+        frutas.ForEach(Console.WriteLine);
+
+        // Alterar um elemento no índice específico -> Substituição
+        frutas[4] = "Pêra";
+
+        Console.WriteLine(); //Pula linha em Branco
+
+        // Impressão da lista em uma única linha
+        frutas.ForEach(Console.WriteLine);
+
+        // Remover elementos da lista do índice específico 
+        frutas.RemoveAt(3);
+
+        // Remover elementos pelo valor do conteúdo
+        frutas.Remove("Morango");
+
+        Console.WriteLine(); //Pula linha em Branco
+
+        // Impressão da lista em uma única linha
+        frutas.ForEach(Console.WriteLine);
+
+        // Apagar todos os elementos da lista
+        frutas.Clear();
+
+        //Minha tentativa falha de função:
+        //FunctionPointerAttributes(frutas);
+        //{
+        //    Console.WriteLine(); //Pula linha em Branco
+        //    Console.WriteLine("Minha lista de frutas:");
+        //    foreach (string fruta in frutas)
+        //    {
+        //        Console.WriteLine(fruta);
+        //    }
+        //}
+
+        // Impressão da lista em uma única linha
+        frutas.ForEach(Console.WriteLine);
+
     }
 }
